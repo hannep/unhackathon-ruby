a0af4cc4a53d9fa310041994f47107a305e3789e
John Daniels <johnsdaniels@gmail.com>
2014-08-02 22:23:43 -0400

    We're mostly just going to use sqlite3 unless we get some huge amount of traffic

--- config/environments.rb
+++ config/environments.rb
@@ -4,16 +4,7 @@
 #This is automatically configured on Heroku, you only need to worry if you also
 #want to run your app locally
 configure :production do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-	ActiveRecord::Base.establish_connection(
-			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
-			:encoding => 'utf8'
-	)
+	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
 end
 
 configure :development do 
